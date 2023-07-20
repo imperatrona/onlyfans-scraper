7c9cf83e796f4a74613bd24c7bc6aaec77b7d4ac
Valentine <xb2w1z@gmail.com>
2023-07-20 19:50:33 +0300

    build

--- dist/index.js
+++ dist/index.js
@@ -12,28 +12,22 @@ var __classPrivateFieldGet = (this && this.__classPrivateFieldGet) || function (
     if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot read private member from an object whose class did not declare it");
     return kind === "m" ? f : kind === "a" ? f.call(receiver) : f ? f.value : state.get(receiver);
 };
-var _Scrapy_instances, _Scrapy_rules, _Scrapy_generateXBc, _Scrapy_init, _Scrapy_createHeaders;
+var __classPrivateFieldSet = (this && this.__classPrivateFieldSet) || function (receiver, state, value, kind, f) {
+    if (kind === "m") throw new TypeError("Private method is not writable");
+    if (kind === "a" && !f) throw new TypeError("Private accessor was defined without a setter");
+    if (typeof state === "function" ? receiver !== state || !f : !state.has(receiver)) throw new TypeError("Cannot write private member to an object whose class did not declare it");
+    return (kind === "a" ? f.call(receiver, value) : f ? f.value = value : state.set(receiver, value)), value;
+};
+var _Scrapy_instances, _Scrapy_rules, _Scrapy_getDynamicRules, _Scrapy_generateXBc, _Scrapy_init, _Scrapy_createHeaders;
 import { createHash } from "crypto";
-import got, { HTTPError } from "got";
+import axios from "axios";
 function sha1(value) {
     return createHash("sha1").update(value).digest("hex");
 }
 class Scrapy {
     constructor(data) {
         _Scrapy_instances.add(this);
-        _Scrapy_rules.set(this, {
-            appToken: "33d57ade8c02dbc5a333db99ff9ae26a",
-            staticParam: "QAIvost4f5b29nagfmKtMqEXjDgRptq0",
-            prefix: "7865",
-            suffix: "6436c06b",
-            checksum: {
-                constant: 72,
-                indexes: [
-                    0, 0, 2, 2, 2, 3, 3, 4, 9, 10, 10, 13, 14, 16, 17, 19, 20, 21, 22, 24,
-                    25, 26, 26, 27, 31, 32, 32, 33, 35, 36, 37, 39,
-                ],
-            },
-        });
+        _Scrapy_rules.set(this, void 0);
         this.auth = {
             userId: "0",
             userAgent: "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36",
@@ -51,26 +45,34 @@ class Scrapy {
                     yield __classPrivateFieldGet(this, _Scrapy_instances, "m", _Scrapy_init).call(this);
                 }
                 catch (error) {
-                    if (error instanceof HTTPError) {
+                    if (error instanceof Error) {
                         console.error(error.message);
                     }
                 }
             }
             const PATH = `/api2/v2/users/${username}`;
-            const data = yield got
-                .get(`https://onlyfans.com${PATH}`, {
+            const req = yield axios.get(`https://onlyfans.com${PATH}`, {
                 headers: __classPrivateFieldGet(this, _Scrapy_instances, "m", _Scrapy_createHeaders).call(this, PATH),
-                agent: this.agent,
-            })
-                .json();
-            return data;
+                httpsAgent: this.agent,
+            });
+            if (req.status !== 200) {
+                throw new Error(`recieve some error on init \n${JSON.stringify(req.data)}`);
+            }
+            return req.data;
         });
     }
     GetSession() {
         return this.auth;
     }
 }
-_Scrapy_rules = new WeakMap(), _Scrapy_instances = new WeakSet(), _Scrapy_generateXBc = function _Scrapy_generateXBc() {
+_Scrapy_rules = new WeakMap(), _Scrapy_instances = new WeakSet(), _Scrapy_getDynamicRules = function _Scrapy_getDynamicRules() {
+    return __awaiter(this, void 0, void 0, function* () {
+        const req = yield axios.get("https://raw.githubusercontent.com/deviint/onlyfans-dynamic-rules/main/dynamicRules.json");
+        if (req.status !== 200)
+            throw new Error(`can't recieve dynamic rules.\nStatus: ${req.status}\nBody: ${req.data}`);
+        return req.data;
+    });
+}, _Scrapy_generateXBc = function _Scrapy_generateXBc() {
     const parts = [
         new Date().getTime(),
         1e12 * Math.random(),
@@ -84,40 +86,41 @@ _Scrapy_rules = new WeakMap(), _Scrapy_instances = new WeakSet(), _Scrapy_genera
     return token;
 }, _Scrapy_init = function _Scrapy_init() {
     return __awaiter(this, void 0, void 0, function* () {
+        __classPrivateFieldSet(this, _Scrapy_rules, yield __classPrivateFieldGet(this, _Scrapy_instances, "m", _Scrapy_getDynamicRules).call(this), "f");
+        if (!__classPrivateFieldGet(this, _Scrapy_rules, "f"))
+            throw new Error("no dynamic rules");
         const PATH = "/api2/v2/init";
-        const data = yield got.get(`https://onlyfans.com${PATH}`, {
+        const req = yield axios.get(`https://onlyfans.com${PATH}`, {
             headers: __classPrivateFieldGet(this, _Scrapy_instances, "m", _Scrapy_createHeaders).call(this, PATH),
-            agent: this.agent,
+            httpsAgent: this.agent,
         });
-        if (data.ok) {
-            const cookies = data.headers["set-cookie"];
-            if (cookies) {
-                this.auth.cookie = cookies
-                    .map((entry) => {
-                    const parts = entry.split(";");
-                    const cookiePart = parts[0];
-                    return cookiePart;
-                })
-                    .join("; ");
-            }
+        if (req.status !== 200) {
+            throw new Error(`recieve some error on init \n${JSON.stringify(req.data)}`);
         }
-        return data;
+        const cookies = req.headers["Set-Cookie"];
+        if (cookies) {
+            this.auth.cookie = cookies.map((entry) => entry.split(";")[0]).join("; ");
+        }
+        return req;
     });
 }, _Scrapy_createHeaders = function _Scrapy_createHeaders(path) {
+    if (!__classPrivateFieldGet(this, _Scrapy_rules, "f"))
+        throw new Error("no dynamic rules provided");
     const time = Date.now().toString();
-    const hash = sha1([__classPrivateFieldGet(this, _Scrapy_rules, "f").staticParam, time, path, this.auth.userId].join("\n"));
-    const checksum = __classPrivateFieldGet(this, _Scrapy_rules, "f").checksum.indexes.reduce((total, current) => total + hash[current].charCodeAt(0), 0) + __classPrivateFieldGet(this, _Scrapy_rules, "f").checksum.constant;
+    const hash = sha1([__classPrivateFieldGet(this, _Scrapy_rules, "f").static_param, time, path, this.auth.userId].join("\n"));
+    const hashAscii = Buffer.from(hash, "ascii");
+    const checksum = __classPrivateFieldGet(this, _Scrapy_rules, "f").checksum_indexes.reduce((total, current) => total + hashAscii[current], 0) + __classPrivateFieldGet(this, _Scrapy_rules, "f").checksum_constant;
     const sign = [
         __classPrivateFieldGet(this, _Scrapy_rules, "f").prefix,
         hash,
-        checksum.toString(16),
+        Math.abs(checksum).toString(16),
         __classPrivateFieldGet(this, _Scrapy_rules, "f").suffix,
     ].join(":");
     if (!this.auth.xBc)
         throw new Error("Unauthorized: xbc not generated");
     const headers = {
         accept: "application/json, text/plain, */*",
-        "app-token": __classPrivateFieldGet(this, _Scrapy_rules, "f").appToken,
+        "app-token": __classPrivateFieldGet(this, _Scrapy_rules, "f").app_token,
         sign: sign,
         time: time,
         "user-agent": this.auth.userAgent,
